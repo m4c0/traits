2505cc235cb55723d93af6b5bf4592d5b6b6d719
Eduardo Costa <m4c0@users.noreply.github.com>
2023-08-09 11:23:55 +0100

    build: using auto-discover

--- build.hpp
+++ build.hpp
@@ -3,11 +3,5 @@
 
 auto traits() {
   using namespace ecow;
-
-  auto m = unit::create<mod>("traits");
-  m->add_part("concepts");
-  m->add_part("invoke");
-  m->add_part("stdint");
-  m->add_part("size_t");
-  return m;
+  return unit::create<mod>("traits");
 }
